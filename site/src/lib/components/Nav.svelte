32dc66bc331bfb3f78c4872c7b338c5a7bf4bfae
Julian Hammer <jh@moinjulian.com>
2023-12-29 00:42:01 +0100

    Add first site content

---
+++ site/src/lib/components/Nav.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+	import ThemeToggler from './ThemeToggler.svelte';
+</script>
+
+<nav>
+	<ul>
+		<li><ThemeToggler></ThemeToggler></li>
+	</ul>
+</nav>
+
+<style lang="scss">
+	nav {
+		padding-block: 1.25rem;
+		background-color: var(--nav-color);
+
+		@media (max-width: 38rem) {
+			padding-block: 0.5rem;
+		}
+
+		ul {
+			list-style-type: none;
+			display: flex;
+			flex-wrap: wrap;
+			justify-content: right;
+			gap: 1.25rem;
+			margin-right: 20px;
+			margin-left: 20px;
+			a {
+				position: relative;
+				text-decoration: none;
+
+				&::before {
+					content: '';
+					position: absolute;
+					left: 0;
+					bottom: -2px;
+					width: 0;
+					height: 2px;
+					background-color: var(--highlight-color);
+					transition: width 0.3s ease;
+				}
+
+				&:hover::before {
+					width: 100%;
+				}
+			}
+
+			li {
+				span {
+					margin-left: 15px;
+					white-space: nowrap;
+				}
+			}
+		}
+	}
+</style>
---
+++ site/src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import Nav from '$lib/components/Nav.svelte';
+	import ScrollUp from '$lib/components/ScrollUp.svelte';
+	import '$lib/scss/app.scss';
+</script>
+
+<svelte:head>
+	<link rel="icon" href="/logo.PNG" />
+</svelte:head>
+
+<ScrollUp></ScrollUp>
+<Nav></Nav>
+
+<main>
+	<slot />
+</main>
+
+<style>
+	main {
+		max-width: 40rem;
+		margin-inline: auto;
+		padding-inline: 1rem;
+	}
+</style>
